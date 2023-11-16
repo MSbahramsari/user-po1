e1f16f832941c4e550943eceef8701b0d4c37ea9
bahramsari <avengebahram82@gmail.com>
2023-11-16 15:18:42 +0330

    remove array and clean

--- updateserver.php
+++ updateserver.php
@@ -1,19 +1,9 @@
 <?php
 extract($_REQUEST , EXTR_PREFIX_SAME , "dup");
-$newData = array();
-//id
-if (intval($id)) {
-    echo $id;
-}
-else {
-    echo 'invalid id';
-}
 echo '<br>';
 //age
 if ($age >= 18) {
     echo $age ;
-    $age = array('age'=>$age);
-    $total = array_merge($newData , $age);
     var_dump($age);
 }
 else {
@@ -24,25 +14,23 @@ echo '<br>';
 //phone number
 if (strlen($phone) <= 11) {
     echo $phone;
-    $phone = array('phone'=>$phone);
-    $total = array_merge($newData , $phone);}
+}
 else {
     echo 'invalid';
 }
 echo '<br>';
 // first name
 if (strlen($firstname) <= 40) {
-    echo $firstname;
-    $firstname = array('firstname'=>$firstname);
-    $total = array_merge($newData , $firstname);}
+    echo $firstname;}
 else {
     echo 'invalid';
 }
 echo '<br>';
 //last name
 if (strlen($lastname) <= 40) {
-    $lastname = array('lastname'=>$lastname);
-    $total = array_merge($newData , $lastname);}
+    echo $lastname ;
+}
+
 
 else {
     echo 'invalid';
@@ -53,25 +41,10 @@ $str = $pwd ;
 $password = md5($str);
 echo $password ;
 echo '<br>';
-$password = array('password'=>$password);
-$total = array_merge($newData , $password);
 
-$gender = array('gender'=>$gender);
-$total = array_merge($newData , $gender);
 
-//put in database
-print_r($total);
-$file = file_get_contents('task.txt');
-$data = json_decode($file, true);
 
-foreach ($data as $key => $user) {
-    if ($user['id'] == $id) {
-        $data[$key] =array_replace($user,$total)
-        ;
 
-    }
-}
-file_put_contents('task.txt',json_encode($data)) ;
 
 
 
