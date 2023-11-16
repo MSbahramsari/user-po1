b6920adbb605133ed5fcee6f2a8bab3a70f81d8d
bahramsari <avengebahram82@gmail.com>
2023-11-16 07:51:30 +0330

    add proje

---
+++ test.php
@@ -0,0 +1,76 @@
+<?php
+extract($_REQUEST , EXTR_PREFIX_SAME , "dup");
+$newData = array();
+//id
+if (intval($id)) {
+    echo $id;
+}
+else {
+    echo 'invalid id';
+}
+echo '<br>';
+//age
+if ($age >= 18) {
+    echo $age ;
+    $age = array('age'=>$age);
+    $total = array_merge($newData , $age);
+}
+else {
+    echo 'invalid age';
+
+}
+echo '<br>';
+//phone number
+if (strlen($phone) <= 11) {
+    echo $phone;
+    $phone = array('phone'=>$phone);
+    $total = array_merge($newData , $phone);}
+else {
+    echo 'invalid';
+}
+echo '<br>';
+// first name
+if (strlen($firstname) <= 40) {
+    echo $firstname;
+    $firstname = array('firstname'=>$firstname);
+    $total = array_merge($newData , $firstname);}
+else {
+    echo 'invalid';
+}
+echo '<br>';
+//last name
+if (strlen($lastname) <= 40) {
+    $lastname = array('lastname'=>$lastname);
+    $total = array_merge($newData , $lastname);}
+
+else {
+    echo 'invalid';
+}
+echo '<br>';
+//password
+$str = $pwd ;
+$password = md5($str);
+echo $password ;
+echo '<br>';
+$password = array('password'=>$password);
+$total = array_merge($newData , $password);
+
+$gender = array('gender'=>$gender);
+$total = array_merge($newData , $gender);
+
+//put in database
+print_r($total);
+$data = fopen("task.txt" , "r+");
+fread( $data , filesize('task.txt'));
+foreach ($data as $key => $user) {
+    if ($user['id'] == $id) {
+        $end[$key] =str_replace($user,$total);
+        fwrite($data , $end);
+        fclose($data);
+
+    }
+}
+
+
+
+
---
+++ update.php
@@ -0,0 +1,76 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="container">
+    <form action="update.php" method="post">
+        <div class="mb-3 mt-3">
+            <label for="number" class="form-label">id</label>
+            <input type="number" class="form-control" name="id">
+        </div>
+        <button type="submit" class="btn btn-outline-primary" name="show">show user</button>
+        <br>
+    </form>
+        <div class="container">
+            <form action="updateserver.php" method="post">
+                <div class="mb-3 mt-3">
+                    <label for="number" class="form-label">id</label>
+                    <input type="number" class="form-control" name="id">
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="text" class="form-label">first name</label>
+                    <input type="text" class="form-control" name="firstname">
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="text" class="form-label">last name</label>
+                    <input type="text" class="form-control"  name="lastname">
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="text" class="form-label">phone number</label>
+                    <input type="number" class="form-control" name="phone">
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="number" class="form-label">age</label>
+                    <input type="number" class="form-control" name="age">
+                </div>
+                <div class="mb-3">
+                    <label for="pwd" class="form-label">Password:</label>
+                    <input type="password" class="form-control" name="pwd">
+                </div>
+                <div><select class="form-select form-select" name="gender">
+                        <option>male</option>
+                        <option>female</option>
+                    </select>
+                </div>
+                <button type="submit" class="btn btn-primary">update</button>
+                <br>
+            </form>
+
+</div>
+
+
+</body>
+</html>
+<?php
+$data = json_decode(file_get_contents('task.txt'),true);
+$id = $_REQUEST["id"];
+$data = array_filter($data, function ($user) use ($id) {if (array_key_exists('show', $_POST)) {
+    if ($user["id"] == $id) {
+        print("<pre>" . print_r($user, true) . "</pre>");
+    }
+}
+    ;});
+
+
+
+//update user
+
+
+
+
