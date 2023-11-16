5950412a662bc3bfd741841520b5cb0c0f55b0de
bahramsari <avengebahram82@gmail.com>
2023-11-16 14:08:22 +0330

    add database and sent values

--- server.php
+++ server.php
@@ -47,6 +47,23 @@ $password = md5($str);
 echo $password ;
 echo '<br>';
 var_dump($gender);
+$servername = "localhost";
+$username = "bahram";
+$password = "123";
+$dbname = "mekeen";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO users (firstname, lastname, age , phone_number , password , gender)
+  VALUES ('$firstname', '$lastname', '$age' ,'$phone' , '$password' , '$gender')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    echo "New record created successfully";
+} catch(PDOException $e) {
+    echo $sql . "<br>" . $e->getMessage();
+}
+$conn = null;
 
 //put in database
 $user = array('gender' => $gender,'password' => $password,'age' => $age,'phone' => $phone,'firstname' => $firstname,'id' => $id,'lastname' =>$lastname) ;
