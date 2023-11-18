701adb0f74f3ba6b147b51d4686006126516bed8
bahramsari <avengebahram82@gmail.com>
2023-11-18 12:27:51 +0330

    create input and html

--- showid.php
+++ showid.php
@@ -1,3 +1,20 @@
-<?php
-$data = json_decode(file_get_contents('task.txt'),true);
-print("<pre>".print_r($data,true)."</pre>");
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+<form action="showid.php">
+    <div class="mb-3 mt-3">
+        <label for="number" class="form-label">id</label>
+        <input type="number" class="form-control" name="id">
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+</body>
+</html>
